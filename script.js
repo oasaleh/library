4de36fa7636cc8618a900e0822a64d217f33f3e0
Omar <omarsaleh92@gmail.com>
2021-01-27 23:19:03 -0600

    Cleaned JS code

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 /* -------------------------------------------------------------------------- */
 /*                               Initialization                               */
 /* -------------------------------------------------------------------------- */
-
-let bookID;
-let newBook;
-let addForm;
-let addButton;
-let addBookButton;
-let closeButton;
-let deleteButton;
-let body;
-let tableHeader;
-let searchBox;
 let myLibrary;
 
-if (localStorage.getItem("myLibrary")) {
+if (localStorage.getItem('myLibrary')) {
   myLibrary = [];
-  let savedLibrary = localStorage.getItem("myLibrary");
+  const savedLibrary = localStorage.getItem('myLibrary');
   myLibrary = JSON.parse(savedLibrary);
 } else {
   myLibrary = [];
 }
-  
 
 // Where books as objects will be stored.
 // const dbLibrary = firebase.database().ref().child("myLibrary");
@@ -56,24 +44,17 @@ if (localStorage.getItem("myLibrary")) {
 /* -------------------------------------------------------------------------- */
 /*                          Grabbing Items from HTML                          */
 /* -------------------------------------------------------------------------- */
-
-// Grab table header row.
-tableHeader = document.getElementById("tableHeader");
-
-// Grab the body.
-body = document.getElementById("body");
-
 // Grab the Open/Add button.
-addButton = document.getElementById("buttonOpenForm");
+const addButton = document.getElementById('buttonOpenForm');
 
 // Grab the close button.
-closeButton = document.getElementById("closeX");
+const closeButton = document.getElementById('closeX');
 
 // Grab the addBookButton button.
-addBookButton = document.getElementById("addBookButton");
+const addBookButton = document.getElementById('addBookButton');
 
 // Grab searchBox
-searchBox = document.getElementById("searchBox");
+const searchBox = document.getElementById('searchBox');
 
 /* -------------------------------------------------------------------------- */
 /*                           Buttons Functionalities                          */
@@ -91,7 +72,7 @@ addBookButton.onclick = addBooks;
 // on Typing start searching
 searchBox.onkeyup = onSearch;
 
-function onSearch(event) {
+function onSearch() {
   const searchText = searchBox.value.toLowerCase();
   findBooks(myLibrary, searchText);
 }
@@ -101,7 +82,10 @@ function onSearch(event) {
 /* -------------------------------------------------------------------------- */
 // Search function
 function findBooks(array, searchText) {
-  const searchArray = array.filter((obj) => obj.title.toLowerCase().includes(searchText) || obj.author.toLowerCase().includes(searchText));
+  const searchArray = array.filter(
+    (obj) => obj.title.toLowerCase().includes(searchText)
+      || obj.author.toLowerCase().includes(searchText),
+  );
   addToHTML(searchArray);
 }
 
@@ -109,22 +93,22 @@ function findBooks(array, searchText) {
 function removeBook(array, uuid) {
   const index = array.findIndex((obj) => obj.id == uuid);
   array.splice(index, 1);
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
   addToHTML(array);
 }
 
 // Hide adding form.
 function hideForm() {
-  document.getElementById("bookTitle").value = "";
-  document.getElementById("authorName").value = "";
-  document.getElementById("bookTitle").placeholder = "Title";
-  document.getElementById("authorName").placeholder = "Author";
-  addBookForm.style.display = "none";
+  document.getElementById('bookTitle').value = '';
+  document.getElementById('authorName').value = '';
+  document.getElementById('bookTitle').placeholder = 'Title';
+  document.getElementById('authorName').placeholder = 'Author';
+  addBookForm.style.display = 'none';
 }
 
 // Display adding form.
 function showForm() {
-  addBookForm.style.display = "block";
+  addBookForm.style.display = 'block';
 }
 // Hide adding form when user clicks outside of adding form box.
 window.onclick = function (event) {
@@ -135,15 +119,18 @@ window.onclick = function (event) {
 
 // Create a book instance of Book and add it to myLibrary array. Then hide the adding form.
 function addBooks() {
-  if (document.getElementById("bookTitle").value == "" || document.getElementById("authorName").value == "") {
-    document.getElementById("bookTitle").placeholder = "Required field!";
-    document.getElementById("authorName").placeholder = "Required field!";
+  if (
+    document.getElementById('bookTitle').value === ''
+    || document.getElementById('authorName').value === ''
+  ) {
+    document.getElementById('bookTitle').placeholder = 'Required field!';
+    document.getElementById('authorName').placeholder = 'Required field!';
   } else {
-    var book = new Book();
-    book.title = document.getElementById("bookTitle").value;
-    book.author = document.getElementById("authorName").value;
+    const book = new Book();
+    book.title = document.getElementById('bookTitle').value;
+    book.author = document.getElementById('authorName').value;
     myLibrary.push(book);
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
     addToHTML(myLibrary);
     hideForm();
   }
@@ -161,27 +148,28 @@ function removeAllChildNodes(parent) {
 // Update HTML and myLibrary with books.
 function addToHTML(array) {
   // Grab the table body.
-  bookTableBody = document.getElementById("bookTableBody");
+  const bookTableBody = document.getElementById('bookTableBody');
   // Remove all books inside the table.
   removeAllChildNodes(bookTableBody);
-  //Loop over the the array myLibrary and create a tr for every object and a td for every title key/value.
+  // Loop over the the array myLibrary and create a tr for every object and a 
+  // td for every title key/value.
 
   array.forEach((obj) => {
     // Create a tr inside the table.
-    let bookRow = document.createElement("tr");
+    const bookRow = document.createElement('tr');
     // Add class book to tr element.
-    bookRow.classList.add("book");
+    bookRow.classList.add('book');
     // Create a td for every title and for every author.
-    let bookTitle = document.createElement("td");
-    let bookAuthor = document.createElement("td");
-    let deleteButton = document.createElement("button");
+    const bookTitle = document.createElement('td');
+    const bookAuthor = document.createElement('td');
+    const deleteButton = document.createElement('button');
     // Add class bookTitle to tr bookTitle element.
-    bookTitle.classList.add("bookTitle");
+    bookTitle.classList.add('bookTitle');
     // Add class bookAuthor to tr bookAuthor element.
-    bookAuthor.classList.add("bookAuthor");
+    bookAuthor.classList.add('bookAuthor');
     // Add class deleteButton to tr deleteButton element.
-    deleteButton.classList.add("deleteButton");
-    deleteButton.setAttribute("id", obj.id);
+    deleteButton.classList.add('deleteButton');
+    deleteButton.setAttribute('id', obj.id);
     deleteButton.onclick = function () {
       removeBook(myLibrary, obj.id);
     };
@@ -192,22 +180,20 @@ function addToHTML(array) {
     // Write the title value into HTML.
     bookTitle.innerHTML = obj.title;
     bookAuthor.innerHTML = obj.author;
-    deleteButton.innerHTML = "&times;";
+    deleteButton.innerHTML = '&times;';
     // Append the bookRow under bookTableBody (the table body.)
     bookTableBody.appendChild(bookRow);
     // saveLibrary();
     // getUpdate();
-
-    
   });
   console.table(myLibrary);
 }
 
 // Generate a unique ID for each Book.
 function uuid() {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8;
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c == 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
@@ -243,4 +229,3 @@ class Book {
 // myLibrary.push(book2);
 // myLibrary.push(book3);
 addToHTML(myLibrary);
-
